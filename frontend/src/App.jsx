cc5acefc3e234eb81744c72f5636f82831dee80e
Supratim Haldar <supratimhaldar05@gmail.com>
2026-09-11 00:56:11 +0530

    fix: replace stadiamaps with free cartodb dark matter tile layer to fix 401 on vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -602,7 +602,7 @@ export default function App() {
           </div>
 
           <MapContainer center={mapCenter} zoom={5} style={{ height: "100%", width: "100%", background: "#0a1128", zIndex: 1 }}>
-            <TileLayer url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png" attribution="&copy; Stadia Maps" />
+            <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors &copy; <a href=&quot;https://carto.com/attributions&quot;>CARTO</a>" />
 
             {showChlHeatmap && chlGrid.map((pt, i) => {
               const color = chlToColor(pt.chl);
